2e23f25a56105000e2d93797b67b13287f470c55
David Gaspar <davidgaspar.dev@gmail.com>
2026-05-22 00:29:32 -0300

    style: Apply Google style pattern

--- tests/main.c
+++ tests/main.c
@@ -1,30 +1,28 @@
-#include <stdio.h>
 #include <bytekit.h>
+#include <stdio.h>
+
 #include "copy_test.c"
-#include "length_test.c"
 #include "index_of_test.c"
-#include "test_equal.c"
+#include "length_test.c"
 #include "test_copy.c"
+#include "test_equal.c"
 
 // Entry point, It is the point at which execution
 // of program is started
-int main(int argc, const char **argv)
-{
-    if (test_equal()) return 1;
-    if (test_copy()) return 1;
-    
-    if (argc > 1)
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            copy_test(argv[i]);
-            length_test(argv[i]);
-            index_of_test(argv[i]);
-        }
+int main(int argc, const char** argv) {
+  if (test_equal()) return 1;
+  if (test_copy()) return 1;
+
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      copy_test(argv[i]);
+      length_test(argv[i]);
+      index_of_test(argv[i]);
     }
+  }
 
-    else
-        printf("You must enter the arguments to perform the tests\n");
+  else
+    printf("You must enter the arguments to perform the tests\n");
 
-    return 0;
+  return 0;
 }
